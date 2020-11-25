5f261458bff66634ad1951bfda5d7460e8ba8b4b
Paulina Carrillo <paulinacarri4@gmail.com>
2020-11-25 20:09:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,7 @@ def using_concat(other_ary1, other_ary2)
   (other_ary1.concat other_ary2).concat
 end
 
-def using_insert(array, element)
+def using_insert(array, integer)
   array.insert(index)
 end
 
