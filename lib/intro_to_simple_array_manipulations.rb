238d763e6a0fdd5b02e8736ae9a48af07e4f7b59
Paulina Carrillo <paulinacarri4@gmail.com>
2020-11-25 20:12:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,7 @@ def using_concat(other_ary1, other_ary2)
 end
 
 def using_insert(array, integer)
-  array.insert(element)
+  array.insert(where_to_insert, what_to_insert)
 end
 
 def using_uniq(array)
